881bd0414ec4225ea5a60677080eba06a5716d1c
swatipatil0807 <sp979519@gamil.com>
2026-06-24 19:26:48 +0530

    Updated Navbar UI

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,11 +2,34 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <nav>
-      <Link to="/">Home</Link>
-      <Link to="/about">About</Link>
-      <Link to="/courses">Courses</Link>
-      <Link to="/contact">Contact</Link>
+    <nav
+      style={{
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+        padding: "15px 40px",
+        background: "#0f172a",
+      }}
+    >
+      <h2 style={{ color: "white" }}>SMS</h2>
+
+      <div style={{ display: "flex", gap: "25px" }}>
+        <Link to="/" style={{ color: "white", textDecoration: "none" }}>
+          Home
+        </Link>
+
+        <Link to="/about" style={{ color: "white", textDecoration: "none" }}>
+          About
+        </Link>
+
+        <Link to="/courses" style={{ color: "white", textDecoration: "none" }}>
+          Courses
+        </Link>
+
+        <Link to="/contact" style={{ color: "white", textDecoration: "none" }}>
+          Contact
+        </Link>
+      </div>
     </nav>
   );
 }
